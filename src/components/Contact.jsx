21409095287694277696bc6e96fdb5e990f5ea6f
Sachinleema <sachinleema347@gmail.com>
2025-01-22 17:33:35 +0530

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { toast } from "react-toastify";
+import { motion } from "motion/react";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "12eec0f6-d7fa-4734-b292-79700e9fcfe0");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message);
+      setResult("");
+    }
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -200 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Contact
+        <span className="underline underline-offset-4 decoration-1 under font-light m-1">
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Ready to Make a Move? Let's Build Your Future Together
+      </p>
+
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8 px-"
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left lg:my-0 md:my-0 sm:my-4">
+            Your Name
+            <input
+              type="text"
+              placeholder="Your Name"
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              name="Name"
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              name="Name"
+            />
+          </div>
+        </div>
+        <div className="my-6 text-left">
+          Message
+          <textarea
+            required
+            name="Message"
+            placeholder="Message"
+            className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded hover:scale-105 tanssition duration-100 ease-in-out">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
+    </motion.div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+  const [showMobileMenu, setshowMobileMenu] = useState(false);
+
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showMobileMenu]);
+
+  return (
+    <div className="absolute top-0 left-0 w-full z-10">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32">
+        <a href="/">
+          <img src={assets.logo} alt="" href="/" />
+        </a>
+        <ul className="hidden md:flex gap-7 text-white">
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#About" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#Projects" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            Testimonials
+          </a>
+        </ul>
+        <button className="hidden md:block bg-white px-8 py-2 rounded-full">
+          Sign up
+        </button>
+        <img
+          src={assets.menu_icon}
+          alt=""
+          className="md:hidden w-7 cursor-pointer"
+          onClick={() => {
+            setshowMobileMenu(true);
+          }}
+        />
+      </div>
+      {/*mobile-menu*/}
+
+      <div
+        className={`md:hidden ${
+          showMobileMenu ? "fixed w-full" : "h-0 w-0"
+        } top-0 right-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <div className="flex justify-end p-6 cursor-pointer">
+          <img
+            src={assets.cross_icon}
+            alt=""
+            className="w-6"
+            onClick={() => {
+              setshowMobileMenu(false);
+            }}
+          />
+        </div>
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          <a
+            href="#Header"
+            className="px-4 py-2 rounded-full inline-block"
+            onClick={() => {
+              setshowMobileMenu(false);
+            }}
+          >
+            Home
+          </a>
+          <a
+            href="#About"
+            className="px-4 py-2 rounded-full inline-block"
+            onClick={() => {
+              setshowMobileMenu(false);
+            }}
+          >
+            About
+          </a>
+          <a
+            href="#Projects"
+            className="px-4 py-2 rounded-full inline-block"
+            onClick={() => {
+              setshowMobileMenu(false);
+            }}
+          >
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="px-4 py-2 rounded-full inline-block"
+            onClick={() => {
+              setshowMobileMenu(false);
+            }}
+          >
+            Testimonials
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
